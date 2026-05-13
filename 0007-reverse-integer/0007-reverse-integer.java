5cf94c0bf009407df1179d5fb95bc84306544f81
shivam1209567 <133603103+shivam1209567@users.noreply.github.com>
2026-05-14 02:38:49 +0530

    Time: 1 ms (99.97%), Space: 42.7 MB (45.05%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,15 +3,12 @@ class Solution {
         int rvrs = 0;
         while (x != 0){
             int digit = x % 10;
-            if(rvrs > Integer.MAX_VALUE / 10 ||
-   (rvrs == Integer.MAX_VALUE / 10 && digit > 7)) {
-    return 0;
-}
-
-if(rvrs < Integer.MIN_VALUE / 10 ||
-   (rvrs == Integer.MIN_VALUE / 10 && digit < -8)) {
-    return 0;
-}
+            if(rvrs > Integer.MAX_VALUE / 10 || (rvrs == Integer.MIN_VALUE / 10 && digit > 7)){
+                return 0;
+            }
+            if(rvrs < Integer.MIN_VALUE / 10 || (rvrs == Integer.MAX_VALUE / 10 && digit < -8)){
+                return 0;
+            }
             rvrs = rvrs * 10 + digit;
             x = x / 10;
             
