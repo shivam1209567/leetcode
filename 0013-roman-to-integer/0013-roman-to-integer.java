40e413298bb77eb5d72a7774cdea6dec3db74858
shivam1209567 <133603103+shivam1209567@users.noreply.github.com>
2026-05-13 22:43:25 +0530

    Time: 3 ms (78.95%), Space: 46.7 MB (29.47%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,25 +1,29 @@
 class Solution {
+    public static int value(char ch){
+        if(ch == 'I') return 1;
+        if(ch == 'V') return 5;
+        if(ch == 'X') return 10;
+        if(ch == 'L') return 50;
+        if(ch == 'C') return 100;
+        if(ch == 'D') return 500;
+        if(ch == 'M') return 1000;
+        return 0;
+    }
     public int romanToInt(String s) {
-    int ans = 0;
-    for(int i = 0; i < s.length(); i++){
-        int curr = getValue(s.charAt(i));
-        if(i < s.length() - 1 && curr < getValue(s.charAt(i+1)) ){
-            ans -= curr;
+      int num = 0;
+      for(int i = 0; i < s.length(); i++){
+        int curr = value(s.charAt(i));
+        if(i+1 < s.length()){
+            int next = value(s.charAt(i+1));
+            if(curr < next){
+                num -= curr;
+            }else{
+                num += curr;
+            }
         }else{
-            ans += curr;
+            num += curr;
         }
+      }
+      return num;
     }
-    
-    return ans;
-    }
-    public static int getValue(char ch){
-        if (ch == 'I') return 1;
-        if (ch == 'V') return 5;
-        if (ch == 'X') return 10;
-        if (ch == 'L') return 50;
-        if (ch == 'C') return 100;
-        if (ch == 'D') return 500;
-        return 1000;
-    }
-        
 }
