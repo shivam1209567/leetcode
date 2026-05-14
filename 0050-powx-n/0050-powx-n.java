3d355c677dd58e6dfcc09c5ffb622d9406bf2c68
shivam1209567 <133603103+shivam1209567@users.noreply.github.com>
2026-05-15 02:57:44 +0530

    Time: 0 ms (100%), Space: 48.3 MB (21.91%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,39 +1,21 @@
 class Solution {
-
-    public double myPow(double x, int n) {
-
-        long N = n;
-
-        if(N < 0){
-
-            x = 1 / x;
-
-            N = -N;
-        }
-
-        return power(x, N);
-    }
-
-    public double power(double x, long n){
-
-        // base case
+    public static double res(double x, long n){
         if(n == 0){
             return 1;
         }
-
-        double half =
-                power(x, n / 2);
-
-        // even power
+        double half = res(x,n/2);
         if(n % 2 == 0){
-
-            return half * half;
+            return half*half;
+        }else{
+            return x*half*half;
         }
-
-        // odd power
-        else{
-
-            return x * half * half;
+    }
+    public double myPow(double x, int n) {
+        long N = n;
+        if(n < 0){
+            x = 1/x;
+            N = -N;
         }
+        return res(x,n);
     }
 }
