9769a5ac8a577dc902f0426be56879356020140b
shivam1209567 <joshishivam2004@gmail.com>
2026-06-14 22:45:15 +0530

    Time: 15 ms (17.01%), Space: 46.9 MB (13.64%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,98 +1,36 @@
-import java.util.*;
-
 class Solution {
-
-    private void evaluate(Stack<Integer> val, Stack<Character> op) {
-        int v2 = val.pop();
-        int v1 = val.pop();
-
-        char sign = op.pop();
-
-        if (sign == '+') {
-            val.push(v1 + v2);
-        } else {
-            val.push(v1 - v2);
-        }
-    }
-
     public int calculate(String s) {
-
-        Stack<Integer> val = new Stack<>();
-        Stack<Character> op = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            if (ch == ' ') continue;
-
-            if (Character.isDigit(ch)) {
-
-                int num = 0;
-
-                while (i < s.length() &&
-                       Character.isDigit(s.charAt(i))) {
-
-                    num = num * 10 +
-                          (s.charAt(i) - '0');
-
-                    i++;
-                }
-
-                i--;
-
-                val.push(num);
+     int sum = 0;
+     int  sign = 1;
+     Stack<Integer> st = new Stack<>();
+     for(int i = 0; i < s.length(); i++){
+        char ch = s.charAt(i);
+        if(ch == ' ') continue;
+        if(Character.isDigit(ch)){
+            int num = 0;
+            while(i < s.length() && Character.isDigit(s.charAt(i))){
+                num = num *10 + (s.charAt(i) - '0');
+                i++;
             }
+            i--;
+            num = num*sign;
+            sum += num;
+            sign = +1;        
+            }else if(ch == '('){
+              st.push(sum);
+              st.push(sign);
+              sum = 0;
+              sign = +1;
 
-            else if (ch == '(') {
-
-                op.push(ch);
+            }else if(ch == ')'){
+                sum *= st.pop();
+                sum += st.pop();
+                
             }
-
-            else if (ch == '+' || ch == '-') {
-
-                // FIX:
-                // Your code failed on:
-                // 1-(     -2)
-                // because it checked only i-1.
-                int j = i - 1;
-
-                while (j >= 0 && s.charAt(j) == ' ') {
-                    j--;
-                }
-
-                if (j < 0 || s.charAt(j) == '(') {
-                    val.push(0);
-                }
-
-                while (!op.isEmpty() &&
-                       op.peek() != '(') {
-
-                    evaluate(val, op);
-                }
-
-                op.push(ch);
+            else if(ch == '-'){
+                sign *= -1;
             }
-
-            else if (ch == ')') {
-
-                while (!op.isEmpty() &&
-                       op.peek() != '(') {
-
-                    evaluate(val, op);
-                }
-
-                if (!op.isEmpty()) {
-                    op.pop();
-                }
-            }
-        }
-
-        while (!op.isEmpty()) {
-
-            evaluate(val, op);
-        }
-
-        return val.pop();
-    }
+     }   
+    return sum;
+}
 }
