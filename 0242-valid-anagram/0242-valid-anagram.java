178ec2f4f7922242f1724e1a9cd1e48499204942
shivam1209567 <joshishivam2004@gmail.com>
2026-06-14 23:23:42 +0530

    Time: 3 ms (91.96%), Space: 44.2 MB (89.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int count[] = new int [26];
+        if(s.length() != t.length()) return false;
+        for(int i = 0; i < s.length(); i++){
+            int l = s.charAt(i) - 'a';
+            count[l]++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            int l = t.charAt(i) - 'a';
+            count[l]--;
+        }
+        for(int i = 0; i < count.length; i++){
+            if(count[i] != 0){
+               return false;
+            }
+        }
+        return true;
+    }
+}
