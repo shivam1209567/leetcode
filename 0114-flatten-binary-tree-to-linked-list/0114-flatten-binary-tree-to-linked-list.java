edfe69e876799244a23a195d0a4351068061c6eb
shivam1209567 <joshishivam2004@gmail.com>
2026-07-06 00:07:23 +0530

    Time: 0 ms (100%), Space: 43.9 MB (70.26%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,35 +1,35 @@
 class Solution {
     public void flatten(TreeNode root) {
-    //     if(root == null ||(root.left == null && root.right == null)) return;
-    //     TreeNode Left = root.left;
-    //     TreeNode Right = root.right;
-    //     root.left = null;
-    //     flatten(Left);
-    //     flatten(Right);
-    //     root.right = Left;
-    //     TreeNode temp = Left;
+        if(root == null ||(root.left == null && root.right == null)) return;
+        TreeNode Left = root.left;
+        TreeNode Right = root.right;
+        root.left = null;
+        flatten(Left);
+        flatten(Right);
+        root.right = Left;
+        TreeNode temp = Left;
         
-    //         while(temp != null && temp.right != null){
-    //             temp = temp.right;
-    //         }
-        
-    //     if(temp != null){
-    //     temp.right = Right;
-    // }else{
-    //     root.right = Right;
-    // }
-    TreeNode curr = root;
-    while(curr != null){
-        if(curr.left != null){
-            TreeNode prev = curr.left;
-            while(prev.right != null){
-                prev = prev.right;
+            while(temp != null && temp.right != null){
+                temp = temp.right;
             }
-            prev.right = curr.right;
-            curr.right = curr.left;
-            curr.left = null;
-        }
-        curr = curr.right;
+        
+        if(temp != null){
+        temp.right = Right;
+    }else{
+        root.right = Right;
     }
+    // TreeNode curr = root;
+    // while(curr != null){
+    //     if(curr.left != null){
+    //         TreeNode prev = curr.left;
+    //         while(prev.right != null){
+    //             prev = prev.right;
+    //         }
+    //         prev.right = curr.right;
+    //         curr.right = curr.left;
+    //         curr.left = null;
+    //     }
+    //     curr = curr.right;
+    // }
     }
 }
