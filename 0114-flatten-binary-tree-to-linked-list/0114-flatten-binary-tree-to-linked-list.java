6f3072fe5432a50eb2030835af18f486577a409a
shivam1209567 <joshishivam2004@gmail.com>
2026-07-06 00:06:51 +0530

    Time: 1 ms (18.53%), Space: 43.8 MB (70.26%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,37 +1,35 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public void flatten(TreeNode root) {
-        if(root == null ||(root.left == null && root.right == null)) return;
-        TreeNode Left = root.left;
-        TreeNode Right = root.right;
-        root.left = null;
-        flatten(Left);
-        flatten(Right);
-        root.right = Left;
-        TreeNode temp = Left;
+    //     if(root == null ||(root.left == null && root.right == null)) return;
+    //     TreeNode Left = root.left;
+    //     TreeNode Right = root.right;
+    //     root.left = null;
+    //     flatten(Left);
+    //     flatten(Right);
+    //     root.right = Left;
+    //     TreeNode temp = Left;
         
-            while(temp != null && temp.right != null){
-                temp = temp.right;
+    //         while(temp != null && temp.right != null){
+    //             temp = temp.right;
+    //         }
+        
+    //     if(temp != null){
+    //     temp.right = Right;
+    // }else{
+    //     root.right = Right;
+    // }
+    TreeNode curr = root;
+    while(curr != null){
+        if(curr.left != null){
+            TreeNode prev = curr.left;
+            while(prev.right != null){
+                prev = prev.right;
             }
-        
-        if(temp != null){
-        temp.right = Right;
-    }else{
-        root.right = Right;
+            prev.right = curr.right;
+            curr.right = curr.left;
+            curr.left = null;
+        }
+        curr = curr.right;
     }
     }
 }
